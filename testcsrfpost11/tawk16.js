2d17a7a62ee31965153dd7a068ffd047bf9eda0f
/hello <czavaleta93@gmail.com>
2021-11-04 14:02:25 -0500

    Update and rename tawk15.js to tawk16.js

--- testcsrfpost11/tawk16.js
+++ testcsrfpost11/tawk16.js
@@ -1,5 +1,6 @@
 
-
+window.onload = function() {
+    
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -10,13 +11,9 @@ s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 
 })();
-    window.Tawk_API.setAttributes({
-        'name'  : 'Name',
-        'email' : 'email@email.com',
-        'hash'  : 'hash value'
-    }, function(error){});
-setTimeout(function() {
+
     window.Tawk_API.popup();
     window.Tawk_API.endChat();
-},5000)
 
+
+}; 
