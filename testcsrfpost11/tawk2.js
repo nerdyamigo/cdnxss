60b4f8c3b2c126f80a1404b50e43ae48ee9781ec
/hello <czavaleta93@gmail.com>
2021-11-04 09:14:03 -0500

    Update and rename tawk.js to tawk2.js

--- testcsrfpost11/tawk2.js
+++ testcsrfpost11/tawk2.js
@@ -1,6 +1,6 @@
 
 
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+/*var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
@@ -10,6 +10,7 @@ s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
 
+*/
 
-
+fetch("https://tawk.to/chat/61830caa6bb0760a494106d7/1fjjuiusl").then(res => console.log(res))
 
