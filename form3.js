1b10f24de44d7b7e377c0faa53847c9c8bb2a188
/hello <czavaleta93@gmail.com>
2021-11-03 19:26:45 -0500

    Update and rename form2.js to form3.js

--- form3.js
+++ form3.js
@@ -5,7 +5,7 @@ var formData = new FormData();
 
 formData.append("csrfmiddlewaretoken", csrf);
 formData.append("creator", "fromBot");
-foremData.append("body", "test1");
+formData.append("body", "test1");
 
 fetch("https://hackedabacus.br2.bughuntr.net/view/9dcacd4c-a0c4-4ed1-9fb6-8f3aba773d8f",{
 	method: 'POST',
