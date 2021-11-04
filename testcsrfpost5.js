a8d71e7b185e014bb89a3350364839c26db75877
/hello <czavaleta93@gmail.com>
2021-11-03 19:49:25 -0500

    Update testcsrfpost5.js

--- testcsrfpost5.js
+++ testcsrfpost5.js
@@ -8,7 +8,7 @@ fetch("http://127.0.0.1:9000/view/c8011b4f-0f8d-48b1-8195-c3914095795f").then((r
 	//formData.append("creator", "fromBot");
 	formData.append("body", data);
 
-	fetch("http://127.0.0.1:9000/view/comment/2b0fb296-f13b-489a-b533-0ca434654d45",{
+	fetch("http://127.0.0.1:9000/comment/2b0fb296-f13b-489a-b533-0ca434654d45",{
 		method: 'POST',
 		body: formData
 	});
