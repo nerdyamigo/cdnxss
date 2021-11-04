47b143d21e590f3d7218640950c61e9602439a4f
/hello <czavaleta93@gmail.com>
2021-11-03 20:35:57 -0500

    Update and rename test14.js to test15.js

--- testcsrfpost11/test15.js
+++ testcsrfpost11/test15.js
@@ -1,7 +1,7 @@
 //fetch("http://127.0.0.1:9000/view/c8011b4f-0f8d-48b1-8195-c3914095795f").then((res) => {
 //	return res.text();
 //}).then(data => {
-document.onload = function () {
+window.onload = function () {
 	var csrf = document.querySelector("[name=csrfmiddlewaretoken]").value;;
 	var formData = new FormData();
 
