b93f0efa4ed2ee5bc4c2bc5eb0e65b5c916c32d2
/hello <czavaleta93@gmail.com>
2021-11-04 10:36:30 -0500

    Update and rename tawk7.js to tawk8.js

--- testcsrfpost11/tawk8.js
+++ testcsrfpost11/tawk8.js
@@ -1,5 +1,4 @@
 
-
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -8,7 +7,7 @@ s1.src='https://embed.tawk.to/61830caa6bb0760a494106d7/1fjjuiusl';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
-});
+})();
 
 
 
